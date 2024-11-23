97cff22bc51e15f174a3386724dc9d34f3eeded5
gibgibbb <diazkristian321@gmail.com>
2024-11-23 14:46:04 +0800

    resolved conflict

--- TravelItineraryPlannerServer/server.js
+++ TravelItineraryPlannerServer/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const employeeRoutes = require('./routes/employeeRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/Employee');
+mongoose.connect('mongodb://localhost:27017/AtlasDB');
 
-app.use('/', employeeRoutes);
+app.use('/', userRoutes);
 
 app.listen(5000, () => {
     console.log('Server has started!');
